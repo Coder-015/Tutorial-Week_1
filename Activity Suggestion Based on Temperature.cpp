524f1b9890df2b52552df58333a97df7b7c23b7e
Tushar Bisht <tb0712@srmist.edu.in>
2025-04-22 04:43:49 +0530

    Create Activity Suggestion Based on Temperature.cpp

---
+++ Activity Suggestion Based on Temperature.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int temp;
+    cout << "Enter temperature: ";
+    cin >> temp;
+
+    if (temp >= 80)
+        cout << "Suggested activity: Swimming" << endl;
+    else if (temp >= 60)
+        cout << "Suggested activity: Tennis" << endl;
+    else if (temp >= 40)
+        cout << "Suggested activity: Golf" << endl;
+    else
+        cout << "Suggested activity: Skiing" << endl;
+
+    return 0;
+}
